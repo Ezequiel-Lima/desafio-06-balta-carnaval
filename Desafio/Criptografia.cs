620426d3e1e4d6c5e57c46eea375c52e2e6052c2
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2023-02-22 10:03:55 -0300

    Add project files.

---
+++ Desafio/Criptografia.cs
@@ -0,0 +1,37 @@
+﻿namespace Desafio
+{
+    public static class Criptografia
+    {
+        private static readonly Dictionary<char, char> tabelaDeCaracteres = new Dictionary<char, char>
+        {
+            {'a', 'K'}, {'b', 'C'}, {'c', 'B'}, {'d', 'A'}, {'e', 'L'}, {'f', 'M'}, {'g', 'N'}, {'h', 'O'},
+            {'i', 'P'}, {'j', 'Q'}, {'k', 'R'}, {'l', 'S'}, {'m', 'D'}, {'n', 'J'}, {'o', 'E'}, {'p', 'F'},
+            {'q', 'G'}, {'r', 'T'}, {'s', 'U'}, {'t', 'H'}, {'u', 'I'}, {'v', 'W'}, {'w', 'V'}, {'x', 'Y'},
+            {'y', 'X'}, {'z', 'Z'},
+        };
+
+        public static string DescriptarTexto(string textoEncriptado)
+        {
+            string textoDescriptado = textoEncriptado.ToLower();
+
+            foreach (var (chave, valor) in tabelaDeCaracteres)
+            {
+                textoDescriptado = textoDescriptado.Replace(valor, chave);
+            }
+
+            return textoDescriptado;
+        }
+
+        public static string EncriptarTexto(string textoDescriptado)
+        {
+            string textoEncriptado = textoDescriptado.ToLower();
+
+            foreach (var (chave, valor) in tabelaDeCaracteres)
+            {
+                textoEncriptado = textoEncriptado.Replace(chave, valor);
+            }
+
+            return textoEncriptado;
+        }
+    }
+}
---
+++ Desafio/Program.cs
@@ -0,0 +1,7 @@
+﻿using Desafio;
+
+Console.WriteLine("Informe o texto: ");
+string texto = Console.ReadLine();
+
+Console.WriteLine(Criptografia.EncriptarTexto(texto));
+Console.WriteLine(Criptografia.DescriptarTexto(texto));
